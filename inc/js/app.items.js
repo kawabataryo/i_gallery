fddbbf01becb824aae183bc7ca8e496ae706a8fe
kawabataryo <ryo.wizard@gmail.com>
2014-08-08 14:25:01 +0900

    不要なメソッドのコメントアウトを削除

--- inc/js/app.items.js
+++ inc/js/app.items.js
@@ -12,7 +12,6 @@
 
 		$.getJSON(json_url, function(data){
 			that._gettable_data = that._data = data;
-			//that.init();
 		});
 
 		// 非同期通信の状態に戻す
@@ -39,10 +38,6 @@
 				return self.indexOf(x) === i;
 			});
 
-			// return $.unique(this._data.map(function(val){
-			// 	return val[attr_key];
-			// }));
-
 		},
 
 		filter : function(conditions){
@@ -58,66 +53,10 @@
 			}
 
 			this._gettable_data = result_data;
-			//this.init();
 
 			return this;
 		}
 
-		// init : function(){
-		// 	this.first_index = 0;
-		// 	this.last_index  = this.get_all().length - 1;
-
-		// 	this.set_index(this.first_index);
-		// },
-
-		// get : function(index){
-		// 	index = this._adjust_index(index);
-		// 	return this._gettable_data[index];
-		// },
-
-		// set_index : function(index){
-		// 	this._index = this._adjust_index(index);
-		// },
-
-		// get_current_index : function(){
-		// 	return this._index;
-		// },
-
-		// get_next_index : function(){
-		// 	return this._adjust_index(this._index + 1);
-		// },
-
-		// get_prev_index : function(){
-		// 	return this._adjust_index(this._index - 1);
-		// },
-
-		// current : function(){
-		// 	return this.get(this.get_current_index());
-		// },
-
-		// next : function(){
-		// 	this.set_index(this.get_next_index());
-		// 	return this.get(this.get_current_index());
-		// },
-
-		// prev : function(){
-		// 	this.set_index(this.get_prev_index());
-		// 	return this.get(this.get_current_index());
-		// },
-
-		// has_next : function(){
-		// 	return (this.get_current_index() < this.last_index);
-		// },
-
-		// has_prev : function(){
-		// 	return (this.get_current_index() > this.first_index);
-		// },
-
-		// _adjust_index : function(index){
-		// 	var first = 0;
-		// 	var last  = this.get_all().length - 1;
-		// 	return Math.min(last, Math.max(first, index));
-		// }
 	};
 
 })(jQuery);
